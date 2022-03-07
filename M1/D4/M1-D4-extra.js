e311239b8939681b2c8a8dc4f718c3a4065d4da3
jcllobet <jcllobet@gmail.com>
2022-03-07 10:00:31 +0100

    extra exercises and today's folder

---
+++ M1/D4/M1-D4-extra.js
@@ -0,0 +1,49 @@
+// Additional assignments for Day 4
+
+/* EXTRA 1
+ Write a piece of code for reverting an array.
+ es:
+ [1, 3, 5] ==> [5, 3, 1]
+*/
+
+
+
+/* EXTRA 2
+ Write a piece of code for getting the maximum numerical value from an array.
+*/
+
+
+
+/* EXTRA 3
+ Write a piece of code for getting the minimum numerical value from an array.
+*/
+
+
+/* EXTRA 4
+ Write a piece of code for getting only even numerical values from an array.
+*/
+
+
+
+/* EXTRA 5
+ Write a piece of code for deleting only even entries from an array.
+*/
+
+
+/* EXTRA 6
+ Write a piece of code for removing all the vowels from a string.
+*/
+
+
+/* EXTRA 7
+ Write a piece of code for increasing all the numerical values in a array by 1.
+*/
+
+/* EXTRA 8 
+ Replace all the strings contained in an array with their length.
+ es.: ["strive", "is", "great"] => [6, 2, 5]
+*/
+
+/* WHEN YOU ARE FINISHED
+ Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
+*/
