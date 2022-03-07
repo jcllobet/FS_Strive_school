e92301c35a4d9c5bbee739d41ae1caf9ceb84370
jcllobet <jcllobet@gmail.com>
2022-03-07 18:09:20 +0100

    fixed address issue
    there it is

--- M1/D6/scripts.js
+++ M1/D6/scripts.js
@@ -2,15 +2,80 @@ console.log("hello world")
 
 // EX7: Write a function to change the h1 text to something else
 
-document.getElementById("shop_title").innerHTML = "NOT an Iphone Repair Shop";
+// textContents is all text contained by an element and all its children that are for formatting purposes only.
+// innerText returns all text contained by an element and all its child elements.
+// innerHtml returns all text, including html tags, that is contained by an element.
+
+// By ID
+
+console.log()
+
+//document.getElementById("shop_title").innerHTML = "NOT an Iphone Repair Shop";
+
+// By Tag
+
+let changeTextBy = (method, element, newText, amount = 'one') => {
+    if (method === 'id') {
+        document.getElementById(element).innerHTML = newText;
+    } else if (method === 'className') {
+        target = document.getElementsByClassName(element);
+        if (amount === 'all') {
+            for (let i = 0; i < target.length; i++) {
+                target[i].innerHTML = newText;
+            }
+        } else {
+            target[0].innerHTML = newText;
+        }
+    } else if (method === 'tagName') {
+        target = document.getElementsByTagName(element);
+        if (amount === 'all') {
+            for (let i = 0; i < target.length; i++) {
+                target[i].innerHTML = newText;
+            }
+        } else {
+            target[0].innerHTML = newText;
+        }
+    } else if (method === 'query') {
+        target = document.querySelector(element);
+        if (amount === 'all') {
+            for (let i = 0; i < target.length; i++) {
+                target[i].innerHTML = newText;
+            }
+        } else {
+            target[0].innerHTML = newText;
+        }
+    } else {
+        console.log('input a valid method')
+    }
+}
+
+changeTextBy('tagName', 'h1', 'NOT an iPhone Repair Shop')
 
 // Write a function to change the page background color
 
-document.body.style.backgroundColor = "#E6E9EB";
+let changeBackgroundColor = (color) => {
+    document.body.style.backgroundColor = color;
+}
+
+changeBackgroundColor("#E6E9EB")
 
 // EX9: Write a function to change the footer address with a fake one
 
-document.getElementsByTagName("footer")[0].innerHTML = "Fake Street 123, Fake City, Fake Country";
+let addressChange = (newStreet) => {
+    // we take the first part!
+    let newAddress = document.getElementsByTagName("footer")[0].children[1].innerHTML.split(':')[0];
+    // append the new address
+    let address = document.getElementsByTagName("footer")[0].children[1].innerHTML = newAddress + ': ' + newStreet;
+}
+
+addressChange("C/ Fake Street 123, Fake City, Fake Country")
+
+console.log(`Query: ${document.querySelector('footer, p').innerHTML}`)
+
+let footerText = document.querySelector('footer, p').innerHTML
+
+console.log(footerText.split(','))
+
 
 // EX10: Write a function to add a CSS class to every Amazon link
 
